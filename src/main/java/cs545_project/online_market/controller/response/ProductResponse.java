9ef0d9e11b9c04322261ca4043bd8f9ead5f85bd
knguyen93 <khanhnt.tdt@gmail.com>
2020-07-10 18:35:39 -0500

    home page UI

---
+++ src/main/java/cs545_project/online_market/controller/response/ProductResponse.java
@@ -0,0 +1,26 @@
+package cs545_project.online_market.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    private List<String> images = new ArrayList<>();
+
+    private List<ReviewResponse> reviews = new ArrayList<>();
+}
