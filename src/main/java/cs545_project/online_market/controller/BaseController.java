88021b8f0e7f0e49fbaec4282a3d23d453848f28
ElsabethA573 <eabreha@mui.edu>
2020-07-10 22:38:01 -0500

    Admin

--- src/main/java/cs545_project/online_market/controller/BaseController.java
+++
@@ -1,7 +0,0 @@
-package cs545_project.online_market.controller;
-
-import cs545_project.online_market.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public abstract class BaseController {
-}
